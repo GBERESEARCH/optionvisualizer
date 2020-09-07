955de7adbae3248fdce3d3378efa8feb8914166b
GBERESEARCH <68221707+GBERESEARCH@users.noreply.github.com>
2020-09-07 18:31:01 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pricer',
+      version='0.0.1',
+      description='Option Pricing, Risk Management and Visualisation tools',
+      author='...',
+      author_email='...',
+      packages=['pricer'],
+      install_requires=['numpy',
+                        'matplotlib',
+			'scipy'])
+
