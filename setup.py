0982f60ce4f1b4c5f1f7c6195e74fd00d3ddd2e2
GBERESEARCH <68221707+GBERESEARCH@users.noreply.github.com>
2020-10-06 10:14:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,35 @@
-from setuptools import setup
+import pathlib
+from setuptools import setup, find_packages
 
-setup(name='pricer',
-      version='0.0.1',
-      description='Option Pricing, Risk Management and Visualisation tools',
-      author='...',
-      author_email='...',
-      packages=['visualizer', 'models'],
-      install_requires=['matplotlib',
-			'numpy',
-                        'plotly',
-			'scipy'])
+HERE = pathlib.Path(__file__).parent
 
+VERSION = '0.1.0'
+PACKAGE_NAME = 'optionvisualizer'
+AUTHOR = '...'
+AUTHOR_EMAIL = '...'
+URL = 'https://github.com/GBERESEARCH/optionvisualizer'
+
+LICENSE = 'MIT'
+DESCRIPTION = 'Option Pricing, Risk Management and Visualisation tools'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'matplotlib',
+      'numpy',
+      'plotly',
+      'scipy'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
